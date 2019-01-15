3a764f4dba275891f6e0c4adc3acb4440966a411
Piotr Nikiel <piotr@nikiel.info>
2019-01-15 18:38:22 +0100

    better example

--- extra/test.py
+++ extra/test.py
@@ -2,8 +2,8 @@ import time
 import readline, rlcompleter
 readline.parse_and_bind("tab: complete")
 import Poverty
-backend_config = '/home/pnikiel/gitProjects/OPCUA-1180_quasar_via_swig/bin/ServerConfig.xml'
-config = '/home/pnikiel/gitProjects/OPCUA-1180_quasar_via_swig/build/bin/config.xml'
+backend_config = '/home/pnikiel/gitProjects/PovertyTest2/bin/ServerConfig.xml'
+config = '/home/pnikiel/gitProjects/PovertyTest2/bin/config.xml'
 Poverty.Poverty.startServer(backend_config, config)
 time.sleep(2)
-t1=Poverty.Poverty_getTemperatureProbe('t1')
+t1=Poverty.Poverty.getTemperatureProbe('t1')
